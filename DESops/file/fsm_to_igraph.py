1f14d5ab9ed4dce58d74cd5bd51d327ba1bd2571
romulo <romulo@umich.edu>
2020-04-14 16:27:11 -0400

    fsm_to_igraph updated. Outputs error messages

--- DESops/file/fsm_to_igraph.py
+++ DESops/file/fsm_to_igraph.py
@@ -1,10 +1,9 @@
+import sys
 # pylint: disable=C0103
 """
 Convert an 'fsm' filetype, which is used/defined by the DESUMA software,
 into an igraph Graph object.
 """
-
-
 def fsm_to_igraph(fsm_filename, g):
     """
     fsm_filename: filename to write output to, e.g. "name_text.fsm"
@@ -41,22 +40,36 @@ def fsm_to_igraph(fsm_filename, g):
     trans_controllable = list()
     trans_prob = list()
 
-    with open(fsm_filename, "r") as f:
+    with open(fsm_filename, 'r') as f:
         # First line in fsm is # of states
         g.add_vertices(int(f.readline()))
+        # next(f)
+        i = 2
         for line in f:
-            if not line or line == "\n":
+            if not line or line == '\n':
+                i+=1
                 continue
             # Should be delimited in the line by tabs
             states_tuple = line.split("\t")
+            if len(states_tuple)<3:
+                sys.exit("ERROR %s:\nMissing argument in line %d\nStates are in the format:\nSOURCE_STATE\tMARKED\t#TRANSITIONS" %(fsm_filename,i) )
             last_el = states_tuple.pop()
-            states_tuple.append(last_el[0:-1])
+            states_tuple.append(last_el[0:-1]) #REMOVING \n
+            # print(states_tuple)
             state_names.append(states_tuple[0])
             state_markings.append(states_tuple[1])
             if len(states_tuple) > 3:
                 state_crit.append(states_tuple[2])
+            i+=1
+            total = 0
             for _ in range(0, int(states_tuple[2])):
                 trans_tuple = f.readline().split("\t")
+                if trans_tuple == ["\n"]:
+                    sys.exit("ERROR %s:\nToo many transitions at state %s" %(fsm_filename, states_tuple[0]))
+                if len(trans_tuple) > 5:
+                    sys.exit("ERROR %s in line %d:\nToo many argument\nTransitions are in the format:\nEVENT\tTARGET_STATE\tc/uc\to/uo\tprob(optional)" %(fsm_filename,i) )
+                if len(trans_tuple) < 4:
+                    sys.exit("ERROR %s in line %d:\nMissing arguments\nTransitions are in the format:\nEVENT\tTARGET_STATE\tc/uc\to/uo\tprob(optional)" %(fsm_filename,i))
                 last_el = trans_tuple.pop()
                 trans_tuple.append(last_el[0:-1])
                 trans_labels.append(trans_tuple[0])
@@ -65,10 +78,19 @@ def fsm_to_igraph(fsm_filename, g):
                 trans_observable.append(trans_tuple[3])
                 if len(trans_tuple) == 5:
                     # probabilistic info encoded
+                    try:
+                        float(trans_tuple[4])
+                    except ValueError:
+                        sys.exit("ERROR %s in line %d:\nProbability value must be a number smaller than or equal to 1" %(fsm_filename,i))
                     trans_prob.append(trans_tuple[4])
+                    total = total + float(trans_tuple[4])
+                i+=1
+            if total > 0 and total != 1:
+                sys.exit("ERROR %s:\nTransitions in state %s do not sum up to 1" %(fsm_filename, states_tuple[0]))
+
 
     # Construct graph
-    g.vs["marked"] = [m == "1" for m in state_markings]
+    g.vs["marked"] = bool(state_markings == '1')
     if state_crit:
         g.vs["crit"] = state_crit
     g.vs["name"] = state_names
@@ -79,9 +101,9 @@ def fsm_to_igraph(fsm_filename, g):
         trans_list_int_names.append((source, target))
     g.add_edges(trans_list_int_names)
     g.es["label"] = trans_labels
-    trans_observable_bool = [x == "o" for x in trans_observable]
+    trans_observable_bool = [x == 'o' for x in trans_observable]
     g.es["obs"] = trans_observable_bool
-    trans_controllable_bool = [x == "c" for x in trans_controllable]
+    trans_controllable_bool = [x == 'c' for x in trans_controllable]
     g.es["contr"] = trans_controllable_bool
 
     if trans_prob:
