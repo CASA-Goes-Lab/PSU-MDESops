3922da8e54e57062e3bab8dc7773ae9b93fcb365
jweitze <jweitze@umich.edu>
2020-05-07 11:22:09 -0400

    changed imports

--- DESops/__init__.py
+++ DESops/__init__.py
@@ -1,6 +1,8 @@
 # flake8: noqa
-from DESops.automata import automata
-from DESops.automata.event import *
-from DESops.automata.state import *
+from DESops.automata.automata import Automata
+from DESops.automata.event.event import Event
+from DESops.automata.state.state import State
+from DESops.file.fsm_to_igraph import read_fsm
+from DESops.file.igraph_to_fsm import write_fsm
 
 __version__ = "20.3.1"
