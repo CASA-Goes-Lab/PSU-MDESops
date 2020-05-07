4e5622ae0b0a72de6a44353d166a083e7f3970e0
jweitze <jweitze@umich.edu>
2020-05-07 11:57:29 -0400

    more file interface

--- DESops/__init__.py
+++ DESops/__init__.py
@@ -4,5 +4,7 @@ from DESops.automata.event.event import Event
 from DESops.automata.state.state import State
 from DESops.file.fsm_to_igraph import read_fsm
 from DESops.file.igraph_to_fsm import write_fsm
+from DESops.file.igraph_to_svg import write_svg
+from DESops.file.igraph_pickle import *
 
 __version__ = "20.3.1"
