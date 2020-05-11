76427fc6a6339260cfe83ecbba82da564e1749dd
jweitze <jweitze@umich.edu>
2020-05-11 15:07:34 -0400

    bug fixed

--- DESops/automata/PFA.py
+++ DESops/automata/PFA.py
@@ -4,7 +4,7 @@ from DESops.automata.automata import _Automata
 class PFA(_Automata):
     """docstring for """
 
-    def __init__(self, init=None, Euc=set(), Euo=set(), E=set(), prob=list()):
+    def __init__(self, init=None, Euc=set(), Euo=set(), E=set(), prob=None):
         super(PFA, self).__init__(init, Euc, Euo, E)
 
         if not prob and self.ecount() > 0:
@@ -13,8 +13,29 @@ class PFA(_Automata):
                 #sys.exit("ERROR: prob attribute not defined for type PFA")
                 warnings.warn("prob attribute not defined for type PFA, setting to default")
 
-        else:
+        elif prob:
             if len(prob) != self.ecount():
                 sys.exit("ERROR: {0} probabilities specified does not match {1} graph ecount".format(len(prob), self.ecount()))
 
             self._graph.es["prob"] = prob.copy()
+        else:
+            self._graph.es["prob"] = []
+            
+        
+    def add_edges(self, pair_list, labels, probs):
+  
+        if len(pair_list) != len(labels):
+            raise IncongruencyError("Length of pairs != length of labels")
+        new_labels = list(self._graph.es["label"])
+        new_labels.extend(labels)
+        
+        self._graph.add_edges(pair_list)
+
+        self._graph.es["label"] = new_labels
+
+        if len(pair_list) != len(probs):
+            raise IncongruencyError("Length of pairs != length of probs")
+        new_probs = list(self._graph.es["prob"])
+        new_probs.extend(probs)
+
+        self._graph.es["prob"] = new_probs
