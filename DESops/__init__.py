a02b71a1475dd8664f10e10dc78cbbb69408da3b
romulo <romulo@umich.edu>
2020-05-26 08:20:01 -0400

    AES and CompactAES

--- DESops/__init__.py
+++ DESops/__init__.py
@@ -1,21 +1,18 @@
 # flake8: noqa
-#from DESops.automata.automata import _Automata
+# from DESops.automata.automata import _Automata
 
 from DESops.automata.DFA import DFA
-from DESops.automata.PFA import PFA
-from DESops.automata.NFA import NFA
-
 from DESops.automata.event.event import Event
-
-from DESops.file.fsm_to_igraph import read_fsm
-from DESops.file.igraph_to_fsm import write_fsm
-from DESops.visualization.write_svg import write_svg
-from DESops.file.igraph_pickle import *
-
-from DESops.visualization.plot import plot
-
+from DESops.automata.NFA import NFA
+from DESops.automata.PFA import PFA
+from DESops.basic_operations.observer_comp import observer_comp
 from DESops.basic_operations.parallel_comp import parallel_comp
 from DESops.basic_operations.product_comp import product_comp
-from DESops.basic_operations.observer_comp import observer_comp
+from DESops.file.fsm_to_igraph import read_fsm
+from DESops.file.igraph_pickle import *
+from DESops.file.igraph_to_fsm import write_fsm
+from DESops.supervisory_control.AES.AES import construct_AES, construct_compact_AES
+from DESops.visualization.plot import plot
+from DESops.visualization.write_svg import write_svg
 
 __version__ = "20.3.1"
--- DESops/supervisory_control/AES/AES.py
+++ DESops/supervisory_control/AES/AES.py
@@ -1,18 +1,18 @@
 import itertools
 import os
 import subprocess
-from pathlib import Path
 
 import igraph as ig
 
+from DESops.automata.DFA import DFA
+from DESops.automata.event.event import Event
+from DESops.basic_operations.ureach import ureach_from_set_adj
 
-def construct_explicit_AES(arena, G, E, Euc, Euo, X_crit, debug=False):
+
+def construct_compact_AES(G, X_crit, debug=False):
     # arena: igraph graph object where resulting arena will be stored, assumed to be empty
-    # G: input system automata, igraph graph object
-    # E: set of events of G
-    # Euc: set of uncontrollable events of G
-    # Euc: set of unobservable events of G
-    # X_crit: safety specification
+    # G: input system automata
+    # X_crit: safety specification based on name of the states
     # debug:   True: print updates for states & time to construct arena
     #          False: no debug information
     #          default: False
@@ -20,144 +20,293 @@ def construct_explicit_AES(arena, G, E, Euc, Euo, X_crit, debug=False):
         import time
 
         start_time = time.process_time()
-    Eo = E - Euo
+
+    # getting vertices of X_crit
+    X_crit = G.vs.select(name_in=X_crit)
+    X_crit = [v.index for v in X_crit]
+    # Q1 and Q2 states map name to vertex index for BTS and set of vertex indices of G; init state is 0
+    Q1, Q2 = dict(), dict()
+    Qname = list()  # holds the names of each state in order of their vertex index
+    # transitions for igraph constructed using vertex index
+    h1, h2 = list(), list()
+    # transitions labels map labels to transitions h1, h2
+    labelh1, labelh2 = list(), list()
+    Q1[frozenset({0})] = 0
+    queue = list()
+    Qname.append(setvs2statename(G, {0}))
+    queue.append({0})
+    construct_Tcomp(G, Qname, Q1, Q2, h1, h2, labelh1, labelh2, queue, X_crit)
+    A = DFA()
+    A.add_vertices(len(Qname), Qname)
+    A.add_edges(h1, labelh1)
+    A.add_edges(h2, labelh2)
+
+    return A
 
 
-#     A1 = set()
-#     # find_A1_sets(A1, G, Euc)
-#     singleton_A1_set(A1,G,E,Euc)
-#     print(A1)
-#     Ea_e = find_Ea_e_events(G, E, Ea)
-#     A2 = Ea_e | Eo
-#     queue = list()
-#     init_state = (frozenset({0}))
-#     queue.append(init_state)
-#     Q1, Q2, h1, h2 = set(), set(), list(), list()
-#     Q1.add(init_state)
-#     adj_dict = dict()
+def construct_AES(G, X_crit, debug=False):
+    # arena: igraph graph object where resulting arena will be stored, assumed to be empty
+    # G: input system automata
+    # X_crit: safety specification based on name of the states
+    # debug:   True: print updates for states & time to construct arena
+    #          False: no debug information
+    #          default: False
+    if debug:
+        import time
 
-#     search_opt(G, Q1, Q2, h1, h2, queue, Eo, Euo, Euc, Ea, A1, A2, X_crit, adj_dict, debug)
-#     # print(adj_dict)
-#     # print(Q1)
-#     # print(len(Q2))
-#     convert_to_graph_opt(arena, G, Q1, Q2, h1, h2, init_state,adj_dict)
-#     Euc_new = {e.name() for e in Ea_e}
-#     Euc_new = Euc_new.union(E)
-#     Euc_new.add(frozenset(Euc))
-#     Euo_new = {e.name() for e in Ea_e}
-#     if debug:
-#         print("-----")
-#         print("Final Vcount: {0}".format(str(arena.vcount())))
-#         print("Time elapsed: {0}".format(str(time.process_time() - start_time)))
-#         print("-----")
+        start_time = time.process_time()
 
-#     return [Euc_new, Euo_new]
+    # getting vertices of X_crit
+    X_crit = G.vs.select(name_in=X_crit)
+    X_crit = [v.index for v in X_crit]
+    # Q1 and Q2 states map name to vertex index for BTS and set of vertex indices of G; init state is 0
+    Q1, Q2 = dict(), dict()
+    Qname = list()  # holds the names of each state in order of their vertex index
+    # transitions for igraph constructed using vertex index
+    h1, h2 = list(), list()
+    # transitions labels map labels to transitions h1, h2
+    labelh1, labelh2 = list(), list()
+    Q1[frozenset({0})] = 0
+    queue = list()
+    Qname.append(setvs2statename(G, {0}))
+    queue.append({0})
+    construct_Tmax(G, Qname, Q1, Q2, h1, h2, labelh1, labelh2, queue, X_crit)
+    A = DFA()
+    A.add_vertices(len(Qname), Qname)
+    A.add_edges(h1, labelh1)
+    A.add_edges(h2, labelh2)
 
-# def search_opt(G, Q1, Q2, h1, h2, queue, Eo, Euo, Euc, Ea, A1, A2, X_crit, adj_dict, debug):
-#     X_crit = set([v.index for v in G.vs.select(name_in = X_crit)])
+    return A
 
-#     while queue:
-#         # print(len(queue))
-#         q = queue.pop(0)
-#         if Q1_state_opt(q):
-#             # Q1 to Q2 transitions
-#             # events = gamma_feasible(G, q, Euc)
-#             events = A1
-#             adj = list()
-#             for e in events:
-#                 target = (UR(q, G, e, Euo), frozenset(e), None)
-#                 h1.append((q, e, target))
-#                 adj.append((target,e))
-#                 add_to_arena_opt(Q1,Q2,target,queue, X_crit,adj_dict, debug)
-#             adj_dict[q] = adj
-#         elif Q2_state_opt(q):
-#             # Various types of Q2 transitions
-#             # print(q)
-#             # print(type(q))
-#             adj = list()
-#             for e in A2:
-#                 if q[2] == e and e in Ea:
-#                     # 2nd type, Q2->Q1
-#                     target = (q[0])
-#                     h2.append((q,e,target))
-#                     add_to_arena_opt(Q1,Q2,target,queue, X_crit,adj_dict, debug)
-#                     adj.append((target,e))
-#                 if q[2] == None:
-#                     if e in Eo and e in G.es(_source_in = q[0])["label"] and e in q[1]:
-#                         # 1st type, Q2->Q1
-#                         target = (NX(e,q[0],G))
-#                         h2.append((q,e,target))
-#                         add_to_arena_opt(Q1,Q2,target,queue, X_crit,adj_dict, debug)
-#                         adj.append((target,e))
-#                     if isinstance(e, Event) and e.inserted and e.label in q[1]:
-#                         # 3rd type, Q2->Q2
-#                         # print('_'.join(e.name()))
-#                         target = (q[0], q[1], M(e))
-#                         h2.append((q,e.name(),target))
-#                         add_to_arena_opt(Q1,Q2,target,queue, X_crit,adj_dict,debug)
-#                         adj.append((target,e.name()))
-#                     if isinstance(e, Event) and e.deleted and e.label in G.es(_source_in = q[0])["label"] and e.label in q[1]:
-#                         # 4th type, Q2->Q2
-#                         # proj is just M(e) since e is in Ead (not in Eai)
-#                         target = (UR(NX(e.label,q[0],G),G,q[1],Euo), q[1], None)
-#                         # if target[0] == frozenset():
-#                             # print(q[1])
-#                             # print(UR(frozenset({3}),G,q[1],Euo))
-#                             # print(e.label)
-#                         h2.append((q,e.name(),target))
-#                         add_to_arena_opt(Q1,Q2,target,queue, X_crit,adj_dict, debug)
-#                         adj.append((target,e.name()))
-#             # print(q)
-#             adj_dict[q] = adj
+    # print(queue)
 
-# def convert_to_graph_opt(arena, G, Q1, Q2, h1, h2, init_state,adj_dict):
-#     # Convert Q1, Q2 states and h1, h2 edges to arena, an igraph Graph
-#     arena.add_vertices(len(Q1)+len(Q2))
-#     # [print(key, value) for key, value in adj_dict.items()]
-#     Q1.remove(init_state)
-#     V_names = list()
-#     V_names.append("{"+G.vs["name"][0]+"}")
-#     E_list = list()
-#     E_events = list()
-#     # Convert Q1 and Q2 into a dict with name : index
-#     adj_list = list()
-#     Q_dict = dict()
-#     Q_dict[init_state] = 0
-#     Q = list()
-#     for i,q in enumerate(Q1,1):
-#         q0_names = "{"+','.join([G.vs["name"][l] for l in q])+"}"
-#         t = (q0_names)
-#         V_names.append(t)
-#         Q_dict[q] = i
-#         Q.append(q)
-#     for j,q in enumerate(Q2,i+1):
-#         q0_names = "{"+','.join([G.vs["name"][l] for l in q[0]])+"}"
-#         q1 = "{"+ ','.join({u for u in q[1]}) + "}"
-#         t = "("+ ','.join([q0_names, q1, str(q[2])])+ ")"
-#         V_names.append(t)
-#         Q_dict[q] = j
-#         Q.append(q)
 
-#     adj_list.append([(Q_dict[v[0]],v[1]) for v in adj_dict[init_state]])
-#     adj_list.extend([[(Q_dict[v[0]],v[1]) for v in adj_dict[Q[i]]] for i,x in enumerate(Q)])
-#     # print(adj_list)
-#     # [print(key, value) for key, value in Q_dict.items()]
-#     # print(V_names)
-#     # print(len(Q1)+len(Q2))
+def construct_Tcomp(G, Qname, Q1, Q2, h1, h2, labelh1, labelh2, queue, X_crit):
+    # index counter saves the current vertex index
+    vertex_counter = 1
+    Eo = G.events - G.Euo
+    UR_state_classes = dict()
+    Gamma = find_compact_control_decisions_sets(G.events, G.Euc, G.Euo)
+    # print(Gamma)
+    while queue:
+        q = queue.pop(0)
+        if Q1_state(q):
+            qvs = q
+            for gamma in Gamma:
+                if not UR_state_classes.get(
+                    (frozenset(qvs), frozenset(G.Euo.intersection(gamma)))
+                ):
+                    q2_state = ureach_from_set_adj(
+                        qvs, G._graph, G.Euo.intersection(gamma)
+                    )
+                    UR_state_classes[
+                        (frozenset(qvs), frozenset(G.Euo.intersection(gamma)))
+                    ] = q2_state
+                # print(setvs2statename(G,q2_state))
+                else:
+                    q2_state = UR_state_classes[
+                        (frozenset(qvs), frozenset(G.Euo.intersection(gamma)))
+                    ]
+                q2 = (q2_state, gamma)
+                vertex_counter = Q2_add_state(
+                    Q1[frozenset(q)],
+                    q2,
+                    G,
+                    Qname,
+                    Q2,
+                    h1,
+                    labelh1,
+                    queue,
+                    vertex_counter,
+                    X_crit,
+                )
 
-#     for h in h1:
-#         source = Q_dict[h[0]]
-#         target = Q_dict[h[2]]
-#         E_list.append((source,target))
-#         E_events.append(h[1])
-#     for h in h2:
-#         source = Q_dict[h[0]]
-#         target = Q_dict[h[2]]
-#         E_list.append((source,target))
-#         E_events.append(h[1])
+        if Q2_state(q):
+            gamma = q[1]
+            states = q[0]
+            # print(gamma,Eo.intersection(gamma))
+            for e in Eo.intersection(gamma):
+                nxstates = {v[0] for i in states for v in G.vs["out"][i] if v[1] == e}
+                print(nxstates)
+                if nxstates:
+                    vertex_counter = Q1_add_state(
+                        nxstates,
+                        Q2[(frozenset(q[0]), frozenset(q[1]))],
+                        e,
+                        G,
+                        Qname,
+                        Q1,
+                        h2,
+                        labelh2,
+                        queue,
+                        vertex_counter,
+                        X_crit,
+                    )
+    print(Qname)
+    print(h1)
 
-#     # print(V_names)
-#     arena.vs["name"] = V_names
-#     arena.add_edges(E_list)
-#     arena.es["label"] = E_events
-#     arena.vs["adj"] = adj_list
+
+def construct_Tmax(G, Qname, Q1, Q2, h1, h2, labelh1, labelh2, queue, X_crit):
+    # index counter saves the current vertex index
+    vertex_counter = 1
+    Eo = G.events - G.Euo
+    UR_state_classes = dict()
+    Gamma = find_control_decisions_sets(G.events, G.Euc)
+    # print(Gamma)
+    while queue:
+        q = queue.pop(0)
+        if Q1_state(q):
+            qvs = q
+            for gamma in Gamma:
+                if not UR_state_classes.get(
+                    (frozenset(qvs), frozenset(G.Euo.intersection(gamma)))
+                ):
+                    q2_state = ureach_from_set_adj(
+                        qvs, G._graph, G.Euo.intersection(gamma)
+                    )
+                    UR_state_classes[
+                        (frozenset(qvs), frozenset(G.Euo.intersection(gamma)))
+                    ] = q2_state
+                # print(setvs2statename(G,q2_state))
+                else:
+                    q2_state = UR_state_classes[
+                        (frozenset(qvs), frozenset(G.Euo.intersection(gamma)))
+                    ]
+                q2 = (q2_state, gamma)
+                vertex_counter = Q2_add_state(
+                    Q1[frozenset(q)],
+                    q2,
+                    G,
+                    Qname,
+                    Q2,
+                    h1,
+                    labelh1,
+                    queue,
+                    vertex_counter,
+                    X_crit,
+                )
+
+        if Q2_state(q):
+            gamma = q[1]
+            states = q[0]
+            # print(gamma,Eo.intersection(gamma))
+            for e in Eo.intersection(gamma):
+                nxstates = {v[0] for i in states for v in G.vs["out"][i] if v[1] == e}
+                print(nxstates)
+                if nxstates:
+                    vertex_counter = Q1_add_state(
+                        nxstates,
+                        Q2[(frozenset(q[0]), frozenset(q[1]))],
+                        e,
+                        G,
+                        Qname,
+                        Q1,
+                        h2,
+                        labelh2,
+                        queue,
+                        vertex_counter,
+                        X_crit,
+                    )
+    print(Qname)
+    print(h1)
+
+
+def Q1_add_state(q1, q2, ev, G, Qname, Q1, h2, labelh2, queue, v_counter, X_crit):
+    if not Q1.get(frozenset(q1)):
+        Q1[frozenset(q1)] = v_counter
+        v = v_counter
+        # If there is not any critical state in q1 state estimate then add to queue
+        if not q1.intersection(X_crit):
+            queue.append(q1)
+        q1name = setvs2statename(G, q1)
+        # print(q1name)
+        Qname.insert(v, q1name)
+        v_counter += 1
+
+    else:
+        v = Q1[frozenset(q1)]
+    h2.append((q2, v))
+    # print(ctr2str(gamma))
+    labelh2.append(ev)
+    return v_counter
+
+
+def Q2_add_state(q1, q2, G, Qname, Q2, h1, labelh1, queue, v_counter, X_crit):
+    if not Q2.get((frozenset(q2[0]), frozenset(q2[1]))):
+        Q2[(frozenset(q2[0]), frozenset(q2[1]))] = v_counter
+        v = v_counter
+        # If there is not any critical state in q2 state estimate then add to queue
+        if not q2[0].intersection(X_crit):
+            queue.append(q2)
+        q2name = ",".join([setvs2statename(G, q2[0]), ctr2str(q2[1])])
+        # print(q2name)
+        Qname.insert(v, "".join(["(", q2name, ")"]))
+        v_counter += 1
+    else:
+        v = Q2[(frozenset(q2[0]), frozenset(q2[1]))]
+    h1.append((q1, v))
+    # print(ctr2str(gamma))
+    labelh1.append(Event(ctr2str(q2[1])))
+    return v_counter
+
+
+def Q1_state(q):
+    return isinstance(q, set)
+
+
+def Q2_state(q):
+    return isinstance(q, tuple)
+
+
+def find_compact_control_decisions_sets(E, Euc, Euo):
+    Ec = set(E - Euc)
+    Ecuo = list(Ec.intersection(Euo))
+    Eco = list(Ec.intersection(E - Euo))
+    GammaEcuo = list()
+    for l in range(len(Ec) + 1):
+        GammaEcuo.extend([Euc.union(comb) for comb in itertools.combinations(Ecuo, l)])
+    Gamma = list()
+    for e in Eco:
+        Gamma.extend([gamma.union({e}) for gamma in GammaEcuo])
+    Gamma.extend(GammaEcuo)
+    print(Gamma)
+    return Gamma
+
+
+def find_control_decisions_sets(E, Euc):
+    Ec = list(E - Euc)
+    print(Ec)
+    Gamma = list()
+    for l in range(len(Ec) + 1):
+        Gamma.extend([Euc.union(comb) for comb in itertools.combinations(Ec, l)])
+    return Gamma
+
+
+def cd2ev(ct):
+    ev = Event()
+    return ev
+
+
+def ctr2str(gamma):
+    name = str()
+    first = True
+    for e in gamma:
+        if first:
+            name = "".join(["{", e.label])
+            first = False
+        else:
+            name = ",".join([name, e.label])
+    return "".join([name, "}"])
+
+
+# transforms a set of vertices to a string with their respective names
+def setvs2statename(G, set_states):
+    name = str()
+    first = True
+    for v in set_states:
+        if first:
+            name = "".join(["{", G.vs["name"][v]])
+            first = False
+        else:
+            name = ",".join([name, G.vs["name"][v]])
+    return "".join([name, "}"])
