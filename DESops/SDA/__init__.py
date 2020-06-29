e2c1c06fcaf81a8cf9db6437c23de5d7332d46b4
jweitze <jweitze@umich.edu>
2020-06-29 16:02:33 -0400

    started updating robust

--- DESops/SDA/__init__.py
+++ DESops/SDA/__init__.py
@@ -2,3 +2,5 @@ from DESops.automata.DFA import DFA
 from DESops.automata.event.event import Event
 from DESops.SDA.AttackSynthesis.construct_AIDA import construct_AIDA as construct_AIDA
 from DESops.SDA.AttackSynthesis.pruning import ISDA as ISDA
+
+# from DESops.SDA.robust.construct_robust import construct_arena_opt_with_attacker
