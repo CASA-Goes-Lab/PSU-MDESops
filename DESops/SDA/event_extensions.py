d9cae85b4063571980a79c241c2ce0be081f094b
jweitze <jweitze@umich.edu>
2020-07-03 11:40:30 -0400

    inserted and deleted functions

---
+++ DESops/SDA/event_extensions.py
@@ -0,0 +1,31 @@
+from DESops.automata.event.event import Event
+
+
+def inserted_event(event):
+    """
+    Creates a new event matching the given label with the
+        insered=True
+        deleted=False
+    Accepts either str (label) or Event as inputs
+    """
+    if isinstance(event, Event):
+        label = event.label
+    else:
+        label = event
+    d = {"inserted": True, "deleted": False}
+    return Event(label, d)
+
+
+def deleted_event(label):
+    """
+    Creates a new event matching the given label with the
+        insered=False
+        deleted=True
+    Accepts either str (label) or Event as inputs
+    """
+    if isinstance(event, Event):
+        label = event.label
+    else:
+        label = event
+    d = {"inserted": False, "deleted": True}
+    return Event(label, d)
