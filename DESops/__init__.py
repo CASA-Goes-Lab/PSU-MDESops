8341a4278ac1d3d7d8568759ee90517cafe0d39d
jweitze <jweitze@umich.edu>
2020-07-13 15:34:29 -0400

    function to extract supervisor

--- DESops/__init__.py
+++ DESops/__init__.py
@@ -9,6 +9,7 @@ from DESops.automata.PFA import PFA
 from DESops.basic_operations import composition, generic_functions, unary
 from DESops.basic_operations.construct_complement import complement
 from DESops.basic_operations.construct_reverse import reverse
+from DESops.basic_operations.language_equivalence import compare_language
 from DESops.basic_operations.observer_comp import observer_comp
 from DESops.basic_operations.parallel_comp import parallel_comp
 from DESops.basic_operations.product_comp import product_comp
@@ -19,7 +20,7 @@ from DESops.file.igraph_to_fsm import write_fsm
 from DESops.generation.generate_automaton import generate_automaton
 from DESops.opacity import opacity
 from DESops.supervisory_control import supervisor
-from DESops.supervisory_control.AES.AES import construct_AES
+from DESops.supervisory_control.AES.AES import construct_AES, extract_AES_super
 from DESops.supervisory_control.supr_contr import supr_contr
 from DESops.supervisory_control.supr_contr_norm import supr_contr_norm
 from DESops.supervisory_control.VLPPO.VLPPO import offline_VLPPO
