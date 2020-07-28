7352e265f6ec3c4344ee9de9d16940a9a8749384
Matthew <matblisc@umich.edu>
2020-07-28 13:12:00 -0400

    NFAs should always have "init" attribute delete_vertices method takes into account initial states

--- DESops/automata/NFA.py
+++ DESops/automata/NFA.py
@@ -6,8 +6,28 @@ from DESops.automata.event import Event
 
 class NFA(_Automata):
     def __init__(self, init=None, Euc=set(), Euo=set(), E=set()):
-        super(NFA, self).__init__(init, Euc, Euo)
-        pass
+        super(NFA, self).__init__(init, Euc, Euo, E)
+
+        if init is None:
+            self.vs["init"] = []
+
+        elif not isinstance(init, NFA):
+            self.vs["init"] = False
+            self.vs[0]["init"] = True
+
+    def delete_vertices(self, vs):
+        self._graph.delete_vertices(vs)
+
+        if not any([v["init"] for v in self.vs]):
+            import warnings
+
+            warnings.warn("All initial states deleted.")
+            self._graph.delete_vertices([v.index for v in self.vs])
+            return
+
+        for state in self.vs:
+            new_out = [(e.target, e["label"]) for e in state.out_edges()]
+            self.vs[state.index].update_attributes({"out": new_out})
 
     def get_destinations(self, state: int, event: Event) -> Set[int]:
         return {out[0] for out in self.vs[state]["out"] if out[1] == event}
